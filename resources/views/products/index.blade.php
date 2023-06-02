f82b663908a29cf67d22bb0611079c5d1237f6b6
TraximUser20 <traximuser20@gmail.com>
2023-06-02 06:02:07 -0700

    Migrations and Controllers Added

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<htm>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        @vite('resources/css/app.css')
+        <title>Laravel CRUD/title>
+    </head>
+</htm>
